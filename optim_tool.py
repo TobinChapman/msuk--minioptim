dd3eaf61dadc78740ea3d1b5caa68aef433cecd4
Tobin Chapman <tobin.chapman@mindshareworld.com>
2024-05-02 11:41:34 +0100

    formatting

--- optim_tool.py
+++ optim_tool.py
@@ -33,6 +33,7 @@ def optimise_curve(df, budget, weeks):
 
 def main():
     st.set_page_config(page_title="KFC Optimiser", page_icon=":dart:", initial_sidebar_state="expanded")
+    st.image('kfc_logo.PNG', width=150)
     st.title("Optimise Budget Across Campaigns")
     st.write("Input budget and number of weeks the media is running. Then select your campaigns:")
 
